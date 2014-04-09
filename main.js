bbb83b76edda97ac15b1716521668cdd665351f3
Carter Nelms <carternelms@bellsouth.net>
2014-04-09 17:29:52 -0500

    version 1.0

--- main.js
+++ main.js
@@ -4,25 +4,46 @@ var active = true;
 
 function withdraw(amount)
 {
-  balance -= amount;
-  if (balance < 0) {
-    balance -= 50;
-    if (balance < -750) {
-      cancel();
-
+  if(isValidAmount(amount))
+  {
+    balance -= amount;
+    if (balance < 0) {
+      var penaltyFee = 50;
+      alert("You have overdrafted and have been charged a $" + penaltyFee + " penalty.");
+      balance -= penaltyFee;
+      if (balance < -750) {
+        cancel();
+      }
     }
   }
 }
 
 function deposit (amount)
 {
-  balance += amount;
-  if (amount >= 500) {
-    balance += 25;
+  if(isValidAmount(amount))
+  {
+    balance += amount;
+    if (amount >= 500) {
+      var bonus = 25;
+      balance += bonus;
+      alert('Congratulations! You have earned a $' + bonus + ' bonus!');
+    }
   }
 }
 
-function quit ()
+function isValidAmount(amount)
+{
+  if(amount)
+  {
+    if(amount > 0)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+function quitATM ()
 {
   quit = true;
   alert("Thank you for banking with us");
@@ -35,7 +56,8 @@ function cancel ()
 }
 
 while (!quit && active) {
-  var transaction_type = prompt('would you like to make a (w)ithdrawal or (d)eposit or (q)uit');
+  var transaction_type = prompt('Current Balance: $' + balance + '\n Welcome \n What would you like to do? \n (w)ithdraw (d)eposit (q)uit');
+  transaction_type = transaction_type.toLowerCase();
   var amount;
   if (transaction_type === "w" || transaction_type === "d") {
     var action;
@@ -45,22 +67,19 @@ while (!quit && active) {
     else {
       action = "depositing";
     }
-    amount = prompt("how much are you " + action + "?");
+    amount = prompt("How much are you " + action + "?") * 1;
   }
   switch (transaction_type){
     case 'w':
       withdraw(amount);
-
       break;
     case 'd':
       deposit (amount);
-      break:
-    case 'q':
-      quit();
       break;
-
+    case 'q':
+      quitATM();
+      break;
     default:
-      alert("nothing");
-      //prompt user if invalid command
+      alert('Please enter a valid command');
   }
 }
